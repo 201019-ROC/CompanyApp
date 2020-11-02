1a394b97a43c38a25eac439c2c197ff681808ca2
vahramkh <vahram.khachikyan@gmail.com>
2020-11-01 20:36:25 -0800

    starting assignment

--- CompanyApp/src/main/java/com/revature/Driver.java
+++ CompanyApp/src/main/java/com/revature/Driver.java
@@ -1,6 +1,10 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import com.revature.services.DepartmentService;
+import com.revature.util.ConnectionUtil;
 
 public class Driver {
 
@@ -8,19 +12,19 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		
-//		try {
-//			
-//			Connection c = ConnectionUtil.getConnection();
-//			String driverName = c.getMetaData().getDriverName();
-//			System.out.println(driverName);
-//			
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			
+			Connection c = ConnectionUtil.getConnection();
+			String driverName = c.getMetaData().getDriverName();
+			System.out.println(driverName);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
 
-//		System.out.println(ds.getAllDepartments());
+		System.out.println(ds.getAllDepartments());
 //		System.out.println(ds.getDepartmentById(1));
 		
 //		Department d = new Department("Coffee", 5000);
